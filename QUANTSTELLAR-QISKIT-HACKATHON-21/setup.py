d52917d8da010df15a517dcbcfbfe543a44b6f5a
RossyNguyen <Rossy.Nhung.Nguyen@ibm.com>
2021-04-25 20:22:58 +0200

    Create ds template w cookiecutter

---
+++ QUANTSTELLAR-QISKIT-HACKATHON-21/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Studies of Star, Galaxy and Quasar Systems Using QSVM',
+    author='QUANTSTELLAR',
+    license='MIT',
+)
